c8afe05cbc818388c3a7a970a4ac7af19d247241
Julien Viet <julien@julienviet.com>
2025-08-23 11:45:35 +0200

    Add annotation processor literal generation

---
+++ vertx-protobuf-it/src/main/java/io/vertx/protobuf/it/SimpleMessage.java
@@ -0,0 +1,20 @@
+package io.vertx.protobuf.it;
+
+import io.vertx.protobuf.annotations.ProtoField;
+import io.vertx.protobuf.annotations.ProtoMessage;
+
+@ProtoMessage
+public class SimpleMessage {
+
+  private String stringField;
+
+  @ProtoField(number = 1, name = "string_field")
+  public String getStringField() {
+    return stringField;
+  }
+
+  public SimpleMessage setStringField(String stringField) {
+    this.stringField = stringField;
+    return this;
+  }
+}
